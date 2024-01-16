f7710a361d62118942d2c2113870c6b16f6c1565
Udo Nonner <udo.nonner@posteo.de>
2024-01-16 13:06:57 +0100

    Improve appearance

--- blocks/include-posts.js
+++ blocks/include-posts.js
@@ -49,318 +49,352 @@ export default function createIncludePostsBlock() {
 			},
 			// --- group: meta
 			{
-				type: 'group',
-				name: 'meta',
-				label: {
-					de: 'Meta-Daten',
-					en: 'Meta Data'
+				type: 'collapsible',
+				label: 'Meta',
+				admin: {
+					initCollapsed: true,
 				},
 				fields: [
 					{
-						type: 'row',
+						type: 'group',
+						name: 'meta',
+						label: ' ',
+						admin: {
+							hideGutter: true,
+							className: 'hide-group-label'
+						},
 						fields: [
-							// --- meta.include
 							{
-								type: 'select',
-								name: 'include',
-								label: {
-									de: 'Welche Meta-Informationen sollen für jeden Post angezeigt werden?',
-									en: 'Which meta data to show for each post?'
-								},
-								hasMany: true,
-								admin: {
-									width: '100%',
-									isClearable: true,
-								},
-								options: [
+								type: 'row',
+								fields: [
+									// --- meta.include
 									{
-										value: 'title',
+										type: 'select',
+										name: 'include',
 										label: {
-											de: 'Titel',
-											en: 'Title'
-										}
+											de: 'Welche Meta-Informationen sollen für jeden Post angezeigt werden?',
+											en: 'Which meta data to show for each post?'
+										},
+										hasMany: true,
+										admin: {
+											width: '100%',
+											isClearable: true,
+										},
+										options: [
+											{
+												value: 'title',
+												label: {
+													de: 'Titel',
+													en: 'Title'
+												}
+											},
+											{
+												value: 'date',
+												label: {
+													de: 'Datum',
+													en: 'Date'
+												}
+											},
+											{
+												value: 'tags',
+												label: {
+													de: 'Tags',
+													en: 'Tags'
+												}
+											},
+											{
+												value: 'summary',
+												label: {
+													de: 'Zusammenfassung',
+													en: 'Summary'
+												}
+											},
+											{
+												value: 'image',
+												label: {
+													de: 'Bild',
+													en: 'Image'
+												}
+											},
+										],
+										defaultValue: ['title', 'date'],
 									},
+									// --- dateStyle
 									{
-										value: 'date',
+										type: 'select',
+										name: 'dateStyle',
 										label: {
-											de: 'Datum',
-											en: 'Date'
-										}
+											de: 'Datumsformat',
+											en: 'Date Style'
+										},
+										admin: {
+											condition: (_, siblingData) => siblingData?.include?.includes('date'),
+											width: '30%',
+										},
+										options: [
+											{
+												value: 'short',
+												label: {
+													de: '22.05.23',
+													en: '5/22/23'
+												}
+											},
+											{
+												value: 'long',
+												label: {
+													de: '22. Mai 2023',
+													en: 'May 22, 2023'
+												}
+											},
+											{
+												value: 'full',
+												label: {
+													de: 'Montag, 22. Mai 2023',
+													en: 'Monday, May 22, 2023'
+												}
+											},
+											{
+												value: 'year',
+												label: {
+													de: '2023',
+													en: '2023'
+												}
+											},
+										],
+										defaultValue: 'short',
 									},
-									{
-										value: 'tags',
+									/* { 
+										type: 'checkbox',
+										name: 'includeTime',
 										label: {
-											de: 'Tags',
-											en: 'Tags'
-										}
-									},
-									{
-										value: 'summary',
-										label: {
-											de: 'Zusammenfassung',
-											en: 'Summary'
-										}
-									},
-									{
-										value: 'image',
-										label: {
-											de: 'Bild',
-											en: 'Image'
-										}
-									},
-								],
-								defaultValue: ['title', 'date'],
+											de: 'Zeit einbeziehen?',
+											en: 'Include Time?'
+										},
+										admin: {
+											condition: (_, siblingData) => siblingData?.useAsTitle === 'date',
+											width: '30%',
+										},
+										defaultValue: false,
+									}, */
+								] // end of row
 							},
-							// --- dateStyle
+							// --- meta.isCollapsible
 							{
-								type: 'select',
-								name: 'dateStyle',
-								label: {
-									de: 'Datumsformat',
-									en: 'Date Style'
-								},
-								admin: {
-									condition: (_, siblingData) => siblingData?.include?.includes('date'),
-									width: '30%',
-								},
-								options: [
-									{
-										value: 'short',
-										label: {
-											de: '22.05.23',
-											en: '5/22/23'
-										}
-									},
-									{
-										value: 'long',
-										label: {
-											de: '22. Mai 2023',
-											en: 'May 22, 2023'
-										}
-									},
-									{
-										value: 'full',
-										label: {
-											de: 'Montag, 22. Mai 2023',
-											en: 'Monday, May 22, 2023'
-										}
-									},
-									{
-										value: 'year',
-										label: {
-											de: '2023',
-											en: '2023'
-										}
-									},
-								],
-								defaultValue: 'short',
-							},
-							/* { 
 								type: 'checkbox',
-								name: 'includeTime',
+								name: 'isCollapsible',
 								label: {
-									de: 'Zeit einbeziehen?',
-									en: 'Include Time?'
+									de: 'Zusammenklappbar',
+									en: 'Collapsible'
+								},
+								defaultValue: true,
+								admin: {
+									condition: (data) => (data.editingMode === 'layout') ? true : false
+								},
+							},
+							// --- meta.initState
+							{
+								type: 'radio',
+								name: 'initState',
+								label: {
+									de: 'Ausgangszustand',
+									en: 'Initial State'
 								},
 								admin: {
-									condition: (_, siblingData) => siblingData?.useAsTitle === 'date',
-									width: '30%',
+									condition: (data, siblingData) => (data.editingMode === 'layout' && siblingData.isCollapsible === true) ? true : false
 								},
-								defaultValue: false,
-							}, */
-						] // end of row
+								options: [
+									{
+										value: 'collapsed',
+										label: {
+											de: 'Zusammengeklappt',
+											en: 'Collapsed'
+										}
+									},
+									{
+										value: 'expanded',
+										label: {
+											de: 'Auseinandergeklappt',
+											en: 'Expanded'
+										}
+									},
+								],
+								defaultValue: 'collapsed',
+							}
+						]
 					},
-					// --- meta.isCollapsible
-					{
-						type: 'checkbox',
-						name: 'isCollapsible',
-						label: {
-							de: 'Zusammenklappbar',
-							en: 'Collapsible'
-						},
-						defaultValue: true,
-						admin: {
-							condition: (data) => (data.editingMode === 'layout') ? true : false
-						},
-					},
-					// --- meta.initState
-					{
-						type: 'radio',
-						name: 'initState',
-						label: {
-							de: 'Ausgangszustand',
-							en: 'Initial State'
-						},
-						admin: {
-							condition: (data, siblingData) => (data.editingMode === 'layout' && siblingData.isCollapsible === true) ? true : false
-						},
-						options: [
-							{
-								value: 'collapsed',
-								label: {
-									de: 'Zusammengeklappt',
-									en: 'Collapsed'
-								}
-							},
-							{
-								value: 'expanded',
-								label: {
-									de: 'Auseinandergeklappt',
-									en: 'Expanded'
-								}
-							},
-						],
-						defaultValue: 'collapsed',
-					}
 				]
 			},
 			// --- group: content
 			{
-				type: 'group',
-				name: 'content',
+				type: 'collapsible',
 				label: {
 					de: 'Inhalt',
 					en: 'Content'
 				},
 				admin: {
-					condition: (data) => (data.editingMode === 'layout') ? true : false
+					initCollapsed: true,
+					condition: (data) => (data.editingMode === 'layout') ? true : false,
 				},
 				fields: [
 					{
-						type: 'row',
+						type: 'group',
+						name: 'content',
+						label: ' ',
+						admin: {
+							hideGutter: true,
+							className: 'hide-group-label'
+						},
 						fields: [
-							// --- includeContentAs
-							/* {
+							{
+								type: 'row',
+								fields: [
+									// --- includeContentAs
+									/* {
+										type: 'radio',
+										name: 'includeAs', // what happens if we click on the post's title?
+										label: {
+											de: 'Wie sollen die Inhalte der Posts eingebunden werden?',
+											en: "How to include the post's content?",
+										},
+										options: [
+											{
+												value: 'rendered',
+												label: 'Rendered'
+											},
+											{
+												value: 'renderedOnView',
+												label: 'Rendered on view'
+											},
+											{
+												value: 'renderedOnClick',
+												label: 'Rendered on click'
+											}
+										],
+										defaultValue: 'rendered',
+									}, */
+									// initRenderedCount
+									/* {
+										type: 'number',
+										name: 'initRenderedCount',
+										label: 'Initially render',
+										defaultValue: 7,
+										admin: {
+											condition: (data, siblingData) => {
+												if (siblingData.includeAs !== 'rendered') {
+													return true;
+												} else {
+													return false;
+												}
+											}
+										}
+									}, */
+								]
+							},
+							// --- content.sortBy
+							{
 								type: 'radio',
-								name: 'includeAs', // what happens if we click on the post's title?
+								name: 'sortBy',
 								label: {
-									de: 'Wie sollen die Inhalte der Posts eingebunden werden?',
-									en: "How to include the post's content?",
+									de: 'Sortierung',
+									en: 'Order',
 								},
 								options: [
 									{
-										value: 'rendered',
-										label: 'Rendered'
-									},
-									{
-										value: 'renderedOnView',
-										label: 'Rendered on view'
-									},
-									{
-										value: 'renderedOnClick',
-										label: 'Rendered on click'
-									}
-								],
-								defaultValue: 'rendered',
-							}, */
-							// initRenderedCount
-							/* {
-								type: 'number',
-								name: 'initRenderedCount',
-								label: 'Initially render',
-								defaultValue: 7,
-								admin: {
-									condition: (data, siblingData) => {
-										if (siblingData.includeAs !== 'rendered') {
-											return true;
-										} else {
-											return false;
+										value: 'date',
+										label: {
+											en: 'Date',
+											de: 'Datum'
 										}
-									}
-								}
-							}, */
+									},
+									{
+										value: 'tags',
+										label: {
+											en: 'Tags',
+											de: 'Tags'
+										}
+									},
+								],
+								defaultValue: 'date',
+							},
 						]
-					},
-					// --- content.sortBy
-					{
-						type: 'radio',
-						name: 'sortBy',
-						label: {
-							de: 'Sortierung',
-							en: 'Order',
-						},
-						options: [
-							{
-								value: 'date',
-								label: {
-									en: 'Date',
-									de: 'Datum'
-								}
-							},
-							{
-								value: 'tags',
-								label: {
-									en: 'Tags',
-									de: 'Tags'
-								}
-							},
-						],
-						defaultValue: 'date',
 					},
 				]
 			},
-			// --- ui
+			// --- group: ui
 			{
-				type: 'group',
-				name: 'ui',
+				type: 'collapsible',
 				label: {
 					de: 'Benutzeroberfläche',
 					en: 'User Interface'
 				},
+				admin: {
+					initCollapsed: true,
+				},
 				fields: [
-					// --- ui.include
 					{
-						type: 'select',
-						name: 'include',
-						label: {
-							de: 'Welche Bedienelemente sollen angezeigt werden?',
-							en: 'Which settings affecting all posts should be shown to the user?'
-						},
-						hasMany: true,
+						type: 'group',
+						name: 'ui',
+						label: ' ',
 						admin: {
-							isClearable: true,
+							hideGutter: true,
+							className: 'hide-group-label'
 						},
-						options: [
+						fields: [
+							// --- ui.include
 							{
-								value: 'tags',
+								type: 'select',
+								name: 'include',
 								label: {
-									de: 'Filter nach Tags',
-									en: 'Filter by tags'
-								}
+									de: 'Welche Bedienelemente sollen angezeigt werden?',
+									en: 'Which settings affecting all posts should be shown to the user?'
+								},
+								hasMany: true,
+								admin: {
+									isClearable: true,
+								},
+								options: [
+									{
+										value: 'tags',
+										label: {
+											de: 'Filter nach Tags',
+											en: 'Filter by tags'
+										}
+									},
+									{
+										value: 'sortorder',
+										label: {
+											de: 'Sortierung',
+											en: 'Sort Order'
+										}
+									},
+									{
+										value: 'searchbar',
+										label: {
+											de: 'Suchleiste',
+											en: 'Searchbar'
+										}
+									},
+									{
+										value: 'toc',
+										label: {
+											de: 'Inhaltsverzeichnis',
+											en: 'Table of Contents'
+										}
+									},
+									{
+										value: 'backToTopButton',
+										label: {
+											de: 'Back-to-Top Button',
+											en: 'Back-to-Top Button'
+										}
+									},
+								],
+								defaultValue: [],
 							},
-							{
-								value: 'sortorder',
-								label: {
-									de: 'Sortierung',
-									en: 'Sort Order'
-								}
-							},
-							{
-								value: 'searchbar',
-								label: {
-									de: 'Suchleiste',
-									en: 'Searchbar'
-								}
-							},
-							{
-								value: 'toc',
-								label: {
-									de: 'Inhaltsverzeichnis',
-									en: 'Table of Contents'
-								}
-							},
-							{
-								value: 'backToTopButton',
-								label: {
-									de: 'Back-to-Top Button',
-									en: 'Back-to-Top Button'
-								}
-							},
-						],
-						defaultValue: [],
+						]
 					},
 				]
-			},
+			}
 		]
 	}
 
