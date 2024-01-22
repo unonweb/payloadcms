2e40678c8987f712f5702f6a8eab66cb2370eafe
Udo Nonner <udo.nonner@posteo.de>
2024-01-22 18:42:49 +0100

    use local api instead http request

--- hooks/getRandomDocID.js
+++ hooks/getRandomDocID.js
@@ -1,7 +1,28 @@
 import log from '../customLog'
 import getRandomInt from './_getRandomInt'
+import payload from 'payload'
 
 export default async function getRandomDocID(colSlug = '', user = '') {
+	/* 
+		Task: Returns a random doc id from the given collection 
+		Limits: Use only on collections without user dependent access control
+	*/
+	
+	try {
+		const result = await payload.find({ // Result will be a paginated set of Posts.
+			collection: colSlug,
+			depth: 0,
+		})
+
+		const randomIndex = getRandomInt(0, result.docs.length)
+		return result.docs[randomIndex].id
+
+	} catch (err) {
+		log(err.stack, user, __filename, 3)
+	}
+}
+
+async function getRandomDocIDFetch(colSlug = '', user = '') {
 	// returns a random doc id from the given collection
 	try {
 		if (user) {
