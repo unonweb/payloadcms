a12551d83b54cbe6e9ce63a4cb9fa9a985534fd5
Udo Nonner <udo.nonner@posteo.de>
2024-01-25 16:22:52 +0100

    add log

--- collections/elements/afterOperationHook.js
+++ collections/elements/afterOperationHook.js
@@ -1,5 +1,13 @@
+import log from '../../customLog'
+import mailError from '../../mailError'
+
 export default async function afterOperationHook(col = '', { args, operation }) {
-	if (['create', 'update', 'updateByID', 'delete', 'deleteByID'].includes(operation)) {
-		console.timeEnd(`<7>[time] [${col}] [total] "${args.req.context.timeID}"`)
+	try {
+		if (['create', 'update', 'updateByID', 'delete', 'deleteByID'].includes(operation)) {
+			console.timeEnd(`<7>[time] [${col}] [total] "${args.req.context.timeID}"`)
+		}	
+	} catch (error) {
+		log(err.stack, user, __filename, 3)
+		mailError(err, req)
 	}
 }
