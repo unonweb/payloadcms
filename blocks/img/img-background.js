d78f70f7347fa59228ae5f1624eb365438a8dfe0
Udo Nonner <udo.nonner@posteo.de>
2024-01-25 17:10:10 +0100

    create (not functional yet)

---
+++ blocks/img/img-background.js
@@ -0,0 +1,51 @@
+import updateCSSObj from '../../hooks/_updateCSSObj'
+import getDoc from '../../hooks/getDoc'
+import updateDocSingle from '../../hooks/updateDocSingle'
+
+export default function createImgBackgroundBlock({ } = {}) {
+
+	const block = {
+		slug: 'img-background',
+		labels: {
+			singular: {
+				en: 'Background Image',
+				de: 'Hintergrundbild'
+			},
+			plural: {
+				en: 'Background Images',
+				de: 'Hintergrundbilder'
+			}
+		},
+		fields: [
+			// --- img.rel
+			{
+				type: 'upload',
+				name: 'rel',
+				label: {
+					de: 'Hintergrundbild',
+					en: 'Background Image'
+				},
+				relationTo: 'images',
+			}
+		],
+		/* hooks: {
+			afterChange: [
+				async ({ originalDoc, value, previousValue }) => {
+					if (value !== previousValue) {
+						
+						const img = await getDoc('images', value, context.user, { depth: 0 })
+						const newCSS = updateCSSObj(context.site.css, 'body', 'background-image', `url(/assets/imgs/$${img.filename}`)
+	
+						updateDocSingle('sites', originalDoc.site, context.user, {
+							data: {
+								css: newCSS
+							}
+						})	
+					}
+				}
+			]
+		} */
+	}
+
+	return block
+}
