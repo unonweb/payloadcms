cf91db1a727b331ce377a4d89dbbb02f36e12dbf
Udo Nonner <udo.nonner@posteo.de>
2024-02-13 12:59:55 +0100

    fix

--- components/deploy-button/index.js
+++ components/deploy-button/index.js
@@ -10,15 +10,15 @@ export const deployButton = () => {
 
 	return (
 		<>
-			<div class="deploy-button-wrapper">
+			<div className="deploy-button-wrapper">
 				<a href={`/deploy-site?site=${siteID}`} target="_blank">
 					<Button
 						className="deploy-button"
 						buttonStyle="secondary">Publish
 					</Button>
 				</a>
-				<div class="field-description">Alle Änderungen hochladen. Das kann etwas dauern.</div>
-				<ul class="field-description">
+				<div className="field-description">Alle Änderungen hochladen. Das kann etwas dauern.</div>
+				<ul className="field-description">
 					<li>Erst speichern</li>
 					<li>Erst am Ende der Sitzung ausführen</li>
 				</ul>
