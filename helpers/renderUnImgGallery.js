389a36fad904f1792f51aa473a1463e382119d98
Udo Nonner <udo.nonner@posteo.de>
2024-02-16 17:18:10 +0100

    - rm link option - refactor attributes

--- helpers/renderUnImgGallery.js
+++ helpers/renderUnImgGallery.js
@@ -5,36 +5,44 @@ module.exports = function renderUnImgGallery(block, meta, context) {
 	
 	const origin = meta.origin
 	const user = context.user
-	
-	const attributes = [
-		// meta
-		(meta.theme) ? `data-theme="${meta.theme}"` : '',
-		(meta.slug) ? `data-page="${meta.slug}"` : '',
-		// block
-		(block.onclick) ? `data-onclick=${block.onclick}` : '',
-		(block.modal) ? `data-modal=${block.modal}` : '',
-		(block.number) ? `data-number=${block.number}` : '',
-		(block.justify) ? `data-justify=${block.justify}` : '',
-		(block.shape) ? `data-shape=${block.shape}` : '',
-		(block.filter) ? `data-filter=${block.filter}` : '',
-		(block.hover?.length > 0) ? `data-hover=${block.hover.join(' ')}` : '', // array!
-	].filter(item => item).join(' ')
+	let HTML = ''
 
-	let html = /* html */`
-		<un-img-gallery ${attributes}>
+	/* attributes */
+	let attributes = {
+		// meta
+		'data-theme': meta.theme,
+		'data-page': meta.id,
+		// block
+		'data-onclick': block.onclick,
+		'data-modal': block.modal,
+		'data-number': block.number,
+		'data-justify': block.justify,
+		'data-shape': block.shape,
+		'data-filter': block.filter,
+		'data-hover': block.hover?.join(' '), // array
+	}
+
+	const attStr = Object.entries(attributes).filter(entry => entry[1]).map(entry => `${entry[0]}='${entry[1]}'`).reduce((prev, curr) => `${prev} ${curr}`, '')
+	
+	/* HTML */
+
+	HTML = /* html */`
+		<un-img-gallery ${attStr}>
 			${block.images.map((img) => {
-				switch (img.link.type) {
-					case 'internal':
-						return /* html */`<a href="${origin}/${img.link.slug}" slot="gallery">${renderImageset(img.rel, meta, context, { sizes: '50vw', loading: 'eager' })}</a>`;
-					case 'custom':
-						return /* html */`<a href="${img.link.url}" slot="gallery">${renderImageset(img.rel, meta, context, { sizes: '50vw', loading: 'eager' })}</a>`;
-					case 'none':
-						return renderImageset(img.rel, meta, context, { sizes: '100vw' }) // 'size: 100vw' as the same img will be used for the modal
-					case 'undefined':
-						log('linkType == undefined', user, __filename, 4);
-				}
+				return renderImageset(img.rel, meta, context, { sizes: '100vw' }) // 'size: 100vw' as the same img will be used for the modal
 			}).join('')}
 		</un-img-gallery>`;
 
-	return html
+	return HTML
 }
+
+/* switch (img.link.type) {
+	case 'internal':
+		return `<a href="${origin}/${img.link.slug}" slot="gallery">${renderImageset(img.rel, meta, context, { sizes: '50vw', loading: 'eager' })}</a>`;
+	case 'custom':
+		return `<a href="${img.link.url}" slot="gallery">${renderImageset(img.rel, meta, context, { sizes: '50vw', loading: 'eager' })}</a>`;
+	case 'none':
+		return renderImageset(img.rel, meta, context, { sizes: '100vw' }) // 'size: 100vw' as the same img will be used for the modal
+	case 'undefined':
+		log('linkType == undefined', user, __filename, 4);
+} */
