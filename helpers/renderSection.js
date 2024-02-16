fa67959bbcde5af8fc313ba67b03014d65f28415
Udo Nonner <udo.nonner@posteo.de>
2024-02-16 17:15:05 +0100

    change but no use

--- helpers/renderSection.js
+++ helpers/renderSection.js
@@ -5,12 +5,12 @@ module.exports = function renderSection(block, meta, context) {
 	let attributes = {
 		// meta
 		'data-theme': meta.theme,
-		'data-page': meta.slug,
+		'data-page': meta.id,
 		// block
 		'data-width': block.width,
 	}
 
-	const attStr = Object.entries(attributes).filter(entry => entry[1]).map(entry => `${entry[0]}='${entry[1]}'`).reduce((prev, curr) => `${prev} ${curr}`)
+	const attStr = Object.entries(attributes).filter(entry => entry[1]).map(entry => `${entry[0]}='${entry[1]}'`).reduce((prev, curr) => `${prev} ${curr}`, '')
 
 	/* for (const attribute of block.attributes ?? []) {
 		attributes.push(`${attribute.key}="${attribute.value}"`)
