8a5acba36b2c2e580eaae4f35bbffc9c5488e053
Udo Nonner <udo.nonner@posteo.de>
2024-02-16 17:14:58 +0100

    change but no use

--- blocks/rich-text-block.js
+++ blocks/rich-text-block.js
@@ -4,6 +4,8 @@ import {
 	UploadFeature,
 	lexicalEditor
 } from '@payloadcms/richtext-lexical'
+import { imgGalleryBlock } from './img/img-gallery'
+import createIncludePostsBlock from './include-posts-block'
 
 const defaultContent = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
@@ -133,7 +135,8 @@ export default function createRichTextBlock() {
 						// directly in the Lexical editor as follows:
 						BlocksFeature({
 							blocks: [
-								//createImgBlock()
+								imgGalleryBlock,
+								createIncludePostsBlock(),
 							],
 						}),
 					]
