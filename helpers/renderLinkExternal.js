97621a0882bfb6f565a8ce5eafdd2f086042b443
Udo Nonner <udo.nonner@posteo.de>
2024-02-16 17:06:10 +0100

    - refactor attributes

--- helpers/renderLinkExternal.js
+++ helpers/renderLinkExternal.js
@@ -3,16 +3,26 @@ module.exports = function renderLinkExternal(block, meta, context) {
 	// NOTES: 
 	// * called by un-menu-bar
 
-	const attributes = [
+	/* const attributes = [
 		// meta
 		(meta.theme) ? `data-theme="${meta.theme}"` : '',
-		(meta.slug) ? `data-page="${meta.slug}"` : '',
 		// block
 		(block.slot) ? `slot="${block.slot}"` : '',
 		(block.link.url) ? `href="${block.link.url}"` : '', // startpage
-	].filter(item => item).join(' ')
+	].filter(item => item).join(' ') */
 
-	let html = /* html */`<a ${attributes}>${block.link.title}</a>`
+	let attributes = {
+		// meta
+		//'data-theme': meta.theme,
+		//'data-page': meta.id,
+		// block
+		'slot': block.slot,
+		'href': block.link.url,
+	}
+
+	const attStr = Object.entries(attributes).filter(entry => entry[1]).map(entry => `${entry[0]}='${entry[1]}'`).reduce((prev, curr) => `${prev} ${curr}`, '')
+
+	let html = /* html */`<a ${attStr}>${block.link.title}</a>`
 
 	return html
 }
