865233aed70f45c624abc04b7887ae1fdf0b15b2
Udo Nonner <udo.nonner@posteo.de>
2024-02-16 17:16:16 +0100

    refactor attributes

--- helpers/renderSVG.js
+++ helpers/renderSVG.js
@@ -1,16 +1,19 @@
 module.exports = function renderSVG(block, meta, context) {
-		
-	const attributes = [
-		(meta.theme) ? `data-theme="${meta.theme}"` : '',
-		(meta.slug) ? `data-page="${meta.slug}"` : '', 
-	].filter(item => item)
 
-	for (const attribute of block.attributes ?? []) {
-		attributes.push(`${attribute.key}="${attribute.value}"`)
+	let attributes = {
+		// meta
+		//'data-theme': meta.theme,
+		'data-page': meta.id,
+	}
+	
+	for (const attObj of block.attributes) {
+		attributes[attObj.key] = attObj.value
 	}
 
+	const attStr = Object.entries(attributes).filter(entry => entry[1]).map(entry => `${entry[0]}='${entry[1]}'`).reduce((prev, curr) => `${prev} ${curr}`, '')
+
 	let html = /* html */`
-		<div ${attributes.join(' ')}>${block.svg}</div>
+		<div ${attStr}>${block.svg}</div>
 	`
 
 	return html
